744260d5e0fd5909a49c65b9595d46a809a84360
Aristeidis Bampakos <bampakoa@gmail.com>
2018-06-15 17:14:41 +0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
+import { Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
@@ -11,6 +12,13 @@ import { AppComponent } from './app.component';
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const AppElement = createCustomElement(AppComponent, { injector: this.injector });
+    customElements.define('ol-zoom-level', AppElement);
+  }
+ }
