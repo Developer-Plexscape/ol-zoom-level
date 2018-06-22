6edfec3f98698ea668a8e300bad9771fcf53222d
Aristeidis Bampakos <bampakoa@gmail.com>
2018-06-22 11:13:15 +0300

    Get zoom level from the map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<div class="ol-control" title="Zoom level">{{zoomLevel}}</div>',
+  styles: [`
+    div {
+      top: 48px;
+      left: 18px;
+      width: 1.375em;
+      height: 1.375em;
+      text-align: center;
+      color: rgba(0,60,136,.5);
+      background-color: transparent !important;
+      font-size: 1.14em;
+      font-weight: 700;
+      display: block;
+      margin: 1px;
+      padding: 0;
+    }
+  `]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  zoomLevel: number;
+
+  @Input() map: ol.Map;
+
+  constructor(private ref: ChangeDetectorRef) {
+    const zoomOutEl: HTMLDivElement = document.querySelector('.ol-zoom-out');
+    zoomOutEl.style.marginTop = '30px';
+  }
+
+  ngOnInit() {
+    this.setZoom(this.map.getView().getZoom());
+
+    this.map.on('moveend', () => {
+      const zoom = this.map.getView().getZoom();
+
+      if (this.zoomLevel !== zoom) { this.setZoom(zoom); }
+    });
+  }
+
+  private setZoom(zoom: number) {
+    this.zoomLevel = zoom;
+    this.ref.detectChanges();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   entryComponents: [AppComponent]
 })
 export class AppModule {
